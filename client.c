b9a2c45cdfe903a3edc091e74e1c5307e8532730
Nicholas <nsayhi@outlook.com>
2022-11-08 23:04:12 -0800

    added comments to client

--- client.c
+++ client.c
@@ -11,7 +11,8 @@
  */
 int initClient(int *sd)
 {
-    *sd = socket(AF_INET, SOCK_STREAM, 0); /* initialize client socket descriptor */
+    /* initialize client socket descriptor */
+    *sd = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in my_address; /* initialize client IP address */
     socklen_t my_address_len = sizeof(my_address);
@@ -22,7 +23,7 @@ int initClient(int *sd)
     /* bind client socket and ip */
     if (bind(*sd, (struct sockaddr *)&my_address, my_address_len) < 0)
     {
-        perror("[ERROR] client-binding error");
+        perror("[ERROR] Client-binding error");
         exit(-1);
     }
     printf("The client is up and running.\n"); /* on-screen message */
@@ -30,18 +31,24 @@ int initClient(int *sd)
     /* get client port number */
     if (getsockname(*sd, (struct sockaddr *)&my_address, &my_address_len) == -1)
     {
-        perror("[Error] client-getsocket name");
+        perror("[Error] Client-getsocket name");
         exit(-1);
     }
 
     return ntohs(my_address.sin_port); /* return client dynamic port number */
 }
 
+/*
+ * Function: connServerM
+ * ----------------------------
+ *   Connect with ServerM via TCP
+ *
+ *   *sd: client socket descriptor
+ */
 void connServerM(int *sd)
 {
+    /* create a socket descriptor connecting to ServerM */
     struct sockaddr_in serverM_address;
-
-    /* create a ServerM socket */
     serverM_address.sin_family = AF_INET;
     serverM_address.sin_addr.s_addr = inet_addr(IP_SERVERM);
     serverM_address.sin_port = htons(PORT_NUM_SERVERM_TCP);
@@ -50,42 +57,64 @@ void connServerM(int *sd)
     if (connect(*sd, (struct sockaddr *)&serverM_address, sizeof(struct sockaddr_in)) < 0)
     {
         close(*sd);
-        perror("[Error] Client to ServerM - connecting stream socket");
+        perror("[Error] Client-connecting socket with ServerM");
         exit(-1);
     }
 }
 
-/* Ask client for Auth input */
+/*
+ * Function: askUserAuth
+ * ----------------------------
+ *   Ask user for authentication input
+ *
+ *   *userAuth: user authentication profile field (e.x. userName or userPsw)
+ *   type: 0-username, 1-password
+ */
 void askUserAuth(char *userAuth, int type)
 {
     char buffer[BUFFSIZE];
     (type) ? (printf("Please enter the password: ")) : (printf("Please enter the username: "));
     fflush(stdout);
-    fgets(buffer, sizeof(buffer), stdin);
+    fgets(buffer, sizeof(buffer), stdin); /* retrieve user input from console */
     buffer[strcspn(buffer, "\n")] = 0;
     strcpy(userAuth, buffer);
-    // printf("input len:%lu.", strlen(userAuth));
 }
 
+/*
+ * Function: sendUserAuth
+ * ----------------------------
+ *   Send user authentication request to ServerM
+ *
+ *   *sd: client socket descriptor
+ *   *newUser: user Auth profile
+ */
 void sendUserAuth(int *sd, struct User_auth *newUser)
 {
+    askUserAuth(newUser->userName, 0); /* ask user for userName */
+    askUserAuth(newUser->userPsw, 1);  /* ask user for userPsw */
 
-    /* Ask client for Auth input */
-    askUserAuth(newUser->userName, 0); // 0: username;
-    askUserAuth(newUser->userPsw, 1);  // 1: password;
-    printf("%s, %s\n", newUser->userName, newUser->userPsw);
-
-    /* Send message(string) to the serverM via TCP */
+    /* Send user authentication to the serverM via TCP */
     if (write(*sd, (struct User_auth *)newUser, sizeof(struct User_auth)) < 0)
         perror("AuthReq send failed");
-
-    printf("%s sent an authentication request to the main server.\n", newUser->userName);
+    printf("%s sent an authentication request to the main server.\n", newUser->userName); /* on-screen message */
 }
 
+/*
+ * Function: recvUserAuthFeedback
+ * ----------------------------
+ *   Receive user authentication result from ServerM
+ *   Send feedback code to caller function "commuServerM()"
+ *
+ *   sd: client socket descriptor
+ *   my_port_num: the dynamic port number assigned to client
+ *   *userName: user Auth profile -> name
+ *   *authAttempts: the remaining number of login attempts
+ *
+ */
 void recvUserAuthFeedback(int sd, int my_port_num, char *userName, int *authAttempts)
 {
     char authFeedback[FEEDBACKSIZE];
-    if (read(sd, &authFeedback, sizeof(authFeedback)) <= 0)
+    if (read(sd, &authFeedback, sizeof(authFeedback)) <= 0) /* receive the auth feedback from serverM */
         perror("Auth Feedback received failed");
     printf("%s received the result of authentication using TCP over port %d. ", userName, my_port_num);
 
@@ -104,7 +133,7 @@ void recvUserAuthFeedback(int sd, int my_port_num, char *userName, int *authAtte
         break;
 
     case 103:
-        *authAttempts = -99;
+        *authAttempts = -99; /* set "-99" as authentication is successful */
         printf("Authentication is successful\n");
         break;
 
@@ -113,30 +142,40 @@ void recvUserAuthFeedback(int sd, int my_port_num, char *userName, int *authAtte
     }
 }
 
+/*
+ * Function: commuServerM
+ * ----------------------------
+ *  Client main chanel with ServerM
+ *  Communicate with ServerM including serveral important sessions:
+ *  e.x. AUTH_SESSION and MAIN_SESSION
+ *
+ *  *sd: client socket descriptor
+ *  my_port_num: the dynamic port number assigned to client
+ */
 void commuServerM(int *sd, int my_port_num)
 {
-    int authAttempts = AUTHATTEMPTS;
-    struct User_auth newUser;
+    int authAttempts = AUTHATTEMPTS; /* maximum login attempts, must greater than 0 */
+    struct User_auth newUser;        /* a User Auth profile */
 
-    /* AUTH_SESSION: send auth msg to server repeatly */
-AUTH_SESSION:
-    sendUserAuth(sd, &newUser);
+AUTH_SESSION:                   /* AUTH_SESSION: process authentication request with serverM */
+    sendUserAuth(sd, &newUser); /* send authentication request to serverM */
     recvUserAuthFeedback(*sd, my_port_num, newUser.userName, &authAttempts);
-    if (authAttempts == 0)
+    if (authAttempts == 0) /* used up all attempts */
     {
         printf("Authentication Failed for %d attempts. Client will shut down.\n", AUTHATTEMPTS);
-        exit(-1);
+        close(*sd); /* close current client socket */
+        exit(-1);   /* terminate current AUTH_SESSION and exit client */
     }
-    else if (authAttempts == -99)
+    else if (authAttempts == -99) /* if auth is successful, goto MAIN_SESSION */
         goto MAIN_SESSION;
-    goto AUTH_SESSION;
+    goto AUTH_SESSION; /* if auth is NOT successful, repeat AUTH_SESSION */
 
-MAIN_SESSION:
+MAIN_SESSION: /* MAIN_SESSION: major tasks */
     printf("Logged In.\n");
 }
 
 /*
- * Function: Main
+ * Function: main
  * ----------------------------
  *  Client entry point
  *  Initialize client and communicate with ServerM
@@ -146,7 +185,7 @@ int main(int argc, char *argv[])
     int sd, my_port_num;            /* client socket descriptor and client port number */
     my_port_num = initClient(&sd);  /* initialize client TCP portal and get dynamic port number */
     connServerM(&sd);               /* connect with serverM via TCP */
-    commuServerM(&sd, my_port_num); /* connect with serverM via TCP */
+    commuServerM(&sd, my_port_num); /* communicate with serverM via TCP */
 
     return 0;
 }
